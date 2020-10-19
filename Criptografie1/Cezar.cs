e68bd984d4987cbc57693a13b9ca9ab9f3101352
Ciubotaru Bogdan <56295736+BogdanCiubotaru24@users.noreply.github.com>
2020-10-19 22:48:13 +0300

    Add files via upload
    First commit

---
+++ Criptografie1/Cezar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Criptografie1
+{
+    class Cezar : AlgoritmSimetric
+    {
+        public Cezar()
+        {
+            criptat = Criptare(text);
+        }
+
+        public string Criptare(string text)
+        {
+            string criptat = "";
+            char c;
+            for(int i = 0; i < text.Length; i++)
+            {
+                c = text[i];
+                if (Char.IsLetter(c))
+                {
+                    if(Char.IsUpper(c))
+                    {
+                        if (c <= 'W')
+                            c += (char)3;
+                        else
+                            c -= (char)23;
+                    }
+                    else
+                        if(c <= 'w')
+                            c += (char)3;
+                        else
+                            c -= (char)23;
+                }
+                criptat += c;
+            }
+            return criptat;
+        }
+
+        public string Decriptare(string text)
+        {
+            string decriptat = "";
+            char c;
+            for (int i = 0; i < text.Length; i++)
+            {
+                c = text[i];
+                if (Char.IsLetter(c))
+                {
+                    if (Char.IsUpper(c))
+                    {
+                        if (c >= 'D')
+                            c -= (char)3;
+                        else
+                            c += (char)23;
+                    }
+                    else
+                        if (c >= 'd')
+                        c -= (char)3;
+                    else
+                        c += (char)23;
+                }
+                decriptat += c;
+            }
+            return decriptat;
+        }
+
+    }
+}
